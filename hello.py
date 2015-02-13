4abb61974454c475e7f978a6791811517ec97d5b
Eric Mill <eric.mill@gsa.gov>
2015-02-13 16:41:37 -0500

    detecting port

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+port = int(os.getenv("VCAP_APP_PORT") or 5000)
 
 @app.route("/")
 def hello():
-    return "Hello, world. I'm in Flask."
+    return ("Hello, world. I'm in Flask, running on port %i." % port)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=port)
